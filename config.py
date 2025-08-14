3283df6d107b0c80a874631be084bcec472f8eb1
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-08-14 09:15:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get("API_ID", "22581733")
+API_ID = os.environ.get("API_ID", "21157244")
 
-API_HASH = os.environ.get("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")
+API_HASH = os.environ.get("API_HASH", "4981c2699bd91c7db836ec8f77e5b0f0)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7643615473:AAFdhOjMKCnpNiiYAsq6uqOrhvb4chaUxEQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
+PASS_DB = int(os.environ.get("PASS_DB", "mongodb+srv://takkishor9784:gG73juoh44MnvJEZ@cluster0.q8hxdk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
 
-#OWNER = int(os.environ.get("OWNER", 6530997270))
+#OWNER = int(os.environ.get("OWNER", 1783306092)
 
-#LOG = -1002487777733,
+#LOG = -1002758652479,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[6530997270]
-    #for x in (os.environ.get("ADMINS", "6530997270").split()):
+    #ADMINS=[1783306092]
+    #for x in (os.environ.get("ADMINS", "1783306092").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
